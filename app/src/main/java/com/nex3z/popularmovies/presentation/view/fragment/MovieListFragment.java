d075e04cdd54d5992ac297c7d9c765b52915cfbf
nex3z <litianxing9@gmail.com>
2017-05-15 22:05:14 +0800

    Fix action bar overlap on the movie list.

--- app/src/main/java/com/nex3z/popularmovies/presentation/view/fragment/MovieListFragment.java
+++ app/src/main/java/com/nex3z/popularmovies/presentation/view/fragment/MovieListFragment.java
@@ -100,7 +100,7 @@ public class MovieListFragment extends BaseFragment implements MovieListView {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_movie_list, container, false);
         mUnbinder = ButterKnife.bind(this, rootView);
         return rootView;
     }
