741c32dde62c38209f1679bba042a1198163fb09
nex3z <litianxing9@gmail.com>
2017-10-12 22:09:02 +0800

    Add basic use case.

---
+++ domain/src/main/java/com/nex3z/popularmovies/domain/model/executor/JobExecutor.java
@@ -0,0 +1,29 @@
+package com.nex3z.popularmovies.domain.model.executor;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class JobExecutor implements ThreadExecutor {
+    private final ThreadPoolExecutor mThreadPoolExecutor;
+
+    public JobExecutor() {
+        mThreadPoolExecutor = new ThreadPoolExecutor(3, 5, 10, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(), new JobThreadFactory());
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+        this.mThreadPoolExecutor.execute(runnable);
+    }
+
+    private static class JobThreadFactory implements ThreadFactory {
+        private int counter = 0;
+
+        @Override
+        public Thread newThread(Runnable runnable) {
+            return new Thread(runnable, "android_" + counter++);
+        }
+    }
+}
